94fa423f38e340af0858b6e428197e36ad91bf06
Deepak Bhankhar <deepakbhankhor758@gmail.com>
2025-06-15 21:17:52 +0530

    "Changed some design and added animations"

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let audioTurn = new Audio("ting.mp3")
-let turn = "X"
+let audioTurn = new Audio("ting.mp3");
+let turn = "X";
 let isgameover = false;
 
 // Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
-}
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+};
 
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
@@ -19,45 +19,70 @@ const checkWin = ()=>{
         [2, 5, 8, 15, 15, 90],
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
+    ];
+
+    for (let e of wins) {
+        if (
+            boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[1]].innerText === boxtext[e[2]].innerText &&
+            boxtext[e[0]].innerText !== ""
+        ) {
+            let winner = boxtext[e[0]].innerText;
+            document.querySelector('.info').innerText = winner + " Won 🎉";
+            isgameover = true;
+
+            // Show winning image
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "180px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+
+
+            // Display line
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
             document.querySelector(".line").style.width = "18vw";
+
+            return;
         }
-    })
-}
+    }
+
+    // Check for draw
+    let filledBoxes = Array.from(boxtext).filter(b => b.innerText !== "").length;
+    if (filledBoxes === 9 && !isgameover) {
+        document.querySelector('.info').innerText = "It's a Draw 🤝";
+        isgameover = true;
+        document.querySelector('.imgbox2').getElementsByTagName('img')[0].style.width = "180px";
+    }
+};
 
 // Game Logic
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '' && !isgameover) {
             boxtext.innerText = turn;
-            turn = changeTurn();
             audioTurn.play();
             checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if (!isgameover) {
+                turn = changeTurn();
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
-    })
-})
+    });
+});
 
-// Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
+// Reset logic
+reset.addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
+        element.innerText = "";
     });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+    turn = "X";
+    isgameover = false;
 
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+
+    // Reset all image widths
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    document.querySelector('.imgbox2').getElementsByTagName('img')[0].style.width = "0px";
+    document.querySelector('.imgboxCelebrate').getElementsByTagName('img')[0].style.width = "0px";
+});
